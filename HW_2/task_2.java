11681907650d45c87cd2a802f21fbede3b383a12
Tina <fatina.kalyanovna@gmail.com>
2023-07-02 00:45:21 +0300

    the task #2  from HW #2 was completed

--- HW_2/task_2.java
+++ HW_2/task_2.java
@@ -1,5 +1,22 @@
 package HW_2;
 
 public class task_2 {
-    
+    public static void dz(int[] intArray) {
+        try {
+
+            int d = 1;
+            double catchedRes1 = (double) intArray[8] / d;
+            if (Double.isInfinite(catchedRes1)) throw new ArithmeticException("infinity");
+            System.out.println("catchedRes1 = " + catchedRes1);
+        } catch (ArithmeticException e) {
+            System.out.println("Catching exception: " + e);
+        } catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("Выход за пределы индекса " + e);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,9,11,23,56};
+        dz(arr);
+    }
 }
